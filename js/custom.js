5af59b8bd43b24f468a3ded3aa9b43554079612e
Ptashkina Liliya <efekta9@gmail.com>
2018-03-07 13:30:56 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -416,19 +416,19 @@ $('.modal-format').validate ({
 /*========================
 scrollbar
  =======================*/
- jQuery('.scrollbar-inner').scrollbar();
+//  jQuery('.scrollbar-inner').scrollbar();
 
- 'use strict';
+//  'use strict';
 
-Tinytest.add('Scrollbar integration', function (test) {
+// Tinytest.add('Scrollbar integration', function (test) {
 
-    var div = document.createElement('div');
-    div.className = 'scrollbar-inner';
-    div.value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in.";
-    var scrollBar = jQuery('.scrollbar-inner').scrollbar();
-    console.log(scrollBar);
-    test.isNotNull(scrollBar, 'instantiation OK');
-});
+//     var div = document.createElement('div');
+//     div.className = 'scrollbar-inner';
+//     div.value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in.";
+//     var scrollBar = jQuery('.scrollbar-inner').scrollbar();
+//     console.log(scrollBar);
+//     test.isNotNull(scrollBar, 'instantiation OK');
+// });
 /*========================
 print
  =======================*/
